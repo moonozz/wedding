86a8d75de2f771077982bbad99f50542cf1fbd2b
moonozz <http0127@naver.com>
2024-08-06 23:12:27 +0900

    :zap: Feat: data 바꾸는 함수를 hook으로  만들어서 다른 컴포넌트에서도 사용하게 하기

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,42 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import data from "../data/data.json";
+import { getKorDate, getWeek, getTime } from "../hooks/filter";
 
 function Title() {
-  const getKorDate = () => {
-    const when = new Date(data.when);
-    const year = when.getFullYear();
-    // const month = ("0" + (when.getMonth()+1)).slice(-2);
-    const month = when.getMonth() + 1;
-    const day = when.getDate();
-
-    return `${year}년 ${month}월 ${day}일`;
-  };
-
-  const getWeek = (i, lan) => {
-    const KorWeek = ["일", "월", "화", "수", "목", "금", "토"];
-    const EngWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    const dayIndex = new Date(i).getDay();
-
-    return lan === "kor" ? `${KorWeek[dayIndex]}요일` : EngWeek[dayIndex];
-  };
-
-  const getTime = (i) => {
-    const time = data.time.split(":");
-
-    if (time[0] < 12) {
-      return `AM ${data.time}`;
-    } else {
-      const timeCal = time[0] - 12;
-      return `PM ${timeCal}:${time[1]}`;
-    }
-  };
-
-  // const getName = (gender) => {
-  //   const male = data.information[0].EngName.split(" ");
-  //   const female = data.information[1].EngName.split(" ");
-  // };
-
   return (
     <TitleSec>
       <TitleDate>
---
+++ src/hooks/filter.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import data from "../data/data.json";
+
+export const getKorDate = () => {
+  const when = new Date(data.when);
+  const year = when.getFullYear();
+  // const month = ("0" + (when.getMonth()+1)).slice(-2);
+  const month = when.getMonth() + 1;
+  const day = when.getDate();
+
+  return `${year}년 ${month}월 ${day}일`;
+};
+
+export const getWeek = (i, lan) => {
+  const KorWeek = ["일", "월", "화", "수", "목", "금", "토"];
+  const EngWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const dayIndex = new Date(i).getDay();
+
+  return lan === "kor" ? `${KorWeek[dayIndex]}요일` : EngWeek[dayIndex];
+};
+
+export const getTime = (i) => {
+  const time = data.time.split(":");
+
+  if (time[0] < 12) {
+    return `AM ${data.time}`;
+  } else {
+    const timeCal = time[0] - 12;
+    return `PM ${timeCal}:${time[1]}`;
+  }
+};
+
+// export default filter
