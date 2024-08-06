8465807f5cad08544484d2d2104ce35c5f88c133
moonozz <http0127@naver.com>
2024-08-06 16:52:14 +0900

    :lipstick: Style: title 영역 작업 및 반응형 작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-// import logo from "./logo.svg";
 import "./App.css";
+import styled from "styled-components";
 import Title from "./components/Title";
 // import data from "./data/data.json";
 
 function App() {
   return (
-    <div className="App">
+    <Main className="w-100 flex-col">
       <Title />
-    </div>
+    </Main>
   );
 }
 
 export default App;
+
+const Main = styled.div`
+  background-color: #fff8f0;
+  /* width: calc(100% - 15.6rem); */
+  max-width: 64rem;
+  min-width: 20rem;
+  padding: 10rem 2rem;
+  text-align: center;
+  font-size: 2rem;
+`;
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,26 +1,94 @@
 import React from "react";
+import styled from "styled-components";
 import data from "../data/data.json";
 
 function Title() {
   return (
-    <div className="Title">
-      <div className="date">
+    <TitleSec>
+      <Date>
         <p>
           {data.when} {data.week}
         </p>
         <p>{data.time}</p>
-      </div>
-      <img
-        src={`${process.env.PUBLIC_URL}/assets/${data.img[0]}`}
-        alt="박보검과 수지의 청첩장 타이틀 이미지"
-      />
-      <div className="txt">
+      </Date>
+      <div className="relative">
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/${data.img[0]}`}
+          alt="박보검과 수지의 청첩장 타이틀 이미지"
+          className="w-100"
+        />
+        {/* <Txt01 className="aoboshi-one flex-col">
+          <span>Happy</span>
+          <span>Wedding</span>
+          <span>Day</span>
+        </Txt01> */}
+        <Txt02 className="aoboshi-one absolute flex-col">
+          <span className="first-title">Happy</span>
+          <span className="second-title">Wedding</span>
+          <span className="third-title">Day</span>
+        </Txt02>
+        {/* <div className="aoboshi-one txt-03">
         <span>Happy</span>
         <span>Wedding</span>
         <span>Day</span>
+      </div> */}
       </div>
-    </div>
+    </TitleSec>
   );
 }
 
 export default Title;
+
+const TitleSec = styled.div`
+  padding: 0 5.8rem;
+  /* height: 85vh; */
+  /* min-height: 110rem; */
+  /* max-height: 120rem; */
+`;
+
+const Date = styled.div`
+  margin-bottom: 5.6rem;
+  text-align: center;
+  p:first-child {
+    font-size: 3.8vw;
+    font-weight: 300;
+  }
+  p:last-child {
+    font-size: 18.5vw;
+    font-weight: 100;
+  }
+
+  @media screen and (min-width: 640px) {
+    margin-bottom: 5.6rem;
+    text-align: center;
+    p:first-child {
+      font-size: 2.4rem;
+      font-weight: 300;
+    }
+    p:last-child {
+      font-size: 12rem;
+      font-weight: 100;
+    }
+  }
+`;
+
+const Txt01 = styled.div`
+  font-size: 16vw;
+  font-weight: 800;
+  color: #de2b2b;
+  @media screen and (min-width: 640px) {
+    font-size: 10rem;
+  }
+`;
+
+const Txt02 = styled.div`
+  top: 9rem;
+  font-size: 16vw;
+  font-weight: 800;
+  color: #de2b2b;
+  @media screen and (min-width: 640px) {
+    font-size: 10rem;
+  }
+`;
+
+const Txt03 = styled.div``;
