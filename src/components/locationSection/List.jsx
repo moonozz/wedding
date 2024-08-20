49b3f8a392da60f40b2f18a4801b48e4a458f310
moonozz <http0127@naver.com>
2024-08-20 22:05:18 +0900

    :zap: Feat: location에 지하철 영역 작업 완료

--- src/components/locationSection/List.jsx
+++ src/components/locationSection/List.jsx
@@ -1,10 +1,96 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import { getSize, getVwSize } from "../../utils/sThemeUtils";
+import { defaultTxtStyle, flex, fontFamily } from "../../utils/sMixinUtils";
+import ColorTag from "./ColorTag";
 
-function List({ title, type, el }) {
-  return <SContainer>List</SContainer>;
+function List({ type, title, el }) {
+  useEffect(() => {
+    console.log(el);
+  }, []);
+
+  return (
+    <SContainer>
+      <STitle>{title}</STitle>
+      {type === "subway" || "parking" ? (
+        <SContent>
+          {el.map((i) => {
+            return (
+              <SContentLi>
+                <SColorTag>
+                  {i.line.map((e) => {
+                    return (
+                      <STagGroup>
+                        <ColorTag lineColor={e} /> <STagP>{e}</STagP>
+                      </STagGroup>
+                    );
+                  })}
+                </SColorTag>
+                <SDiscript>{i.discript}</SDiscript>
+              </SContentLi>
+            );
+          })}
+        </SContent>
+      ) : (
+        <ColorTag>{}</ColorTag>
+      )}
+    </SContainer>
+  );
 }
 
 export default List;
 
-const SContainer = styled.div``;
+const SContainer = styled.div`
+  border-top: 0.5px solid rgb(0, 0, 0, 0.2);
+  padding: ${getVwSize(60)} 0;
+  margin: 0 ${getSize(20)};
+  ${flex("column", "flex-start", "")};
+  ${fontFamily("MaruBuri", "")};
+
+  @media screen and (min-width: 640px) {
+    padding: ${getSize(60)} 0;
+  }
+`;
+
+const STitle = styled.p`
+  font-size: ${getVwSize(24)};
+  margin-bottom: ${getVwSize(32)};
+  font-weight: 900;
+
+  @media screen and (min-width: 640px) {
+    font-size: ${getSize(24)};
+    margin-bottom: ${getSize(40)};
+  }
+`;
+
+const SContent = styled.ul`
+  ${defaultTxtStyle("", "20", "")};
+`;
+
+const SContentLi = styled.li`
+  ${flex("column", "", "")};
+`;
+
+const SColorTag = styled.div`
+  ${flex("row", "", "")};
+`;
+
+const STagGroup = styled.div`
+  ${flex("row", "", "")};
+`;
+
+const SDiscript = styled.p`
+  text-align: left;
+  line-height: 1.8;
+  padding: ${getVwSize(10)} 0 0 ${getVwSize(26)};
+  margin-bottom: ${getVwSize(32)};
+
+  @media screen and (min-width: 640px) {
+    padding-left: ${getSize(24)};
+    margin-bottom: ${getSize(40)};
+  }
+`;
+
+const STagP = styled.p`
+  margin-right: ${getSize(8)};
+`;
--- src/section/Location.jsx
+++ src/section/Location.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import data from "../data/data.json";
 import SubTitle from "../components/SubTitle";
-import { boxSize } from "../utils/sMixinUtils";
+import List from "../components/locationSection/List";
+import {
+  boxSize,
+  flex,
+  fontFamily,
+  defaultTxtStyle,
+  paddingIgnore,
+} from "../utils/sMixinUtils";
 import { getSize, getVwSize } from "../utils/sThemeUtils";
 
 function Location() {
+  const locationData = data.location;
+
   return (
     <SContainer>
       <SubTitle text={"Location"} />
+      <SAddress>
+        <SHallName>{locationData.weddingHall}</SHallName>
+        <SHallAddress>{locationData.address}</SHallAddress>
+      </SAddress>
+      <SMap />
+      <List type={"subway"} title={"지하철"} el={locationData.subway} />
     </SContainer>
   );
 }
@@ -16,5 +32,41 @@ export default Location;
 
 const SContainer = styled.div`
   ${boxSize("100%", "auto")};
+  ${flex("column", "", "")};
+  ${fontFamily("MaruBuri", "")}
   padding: ${getSize(60)} 0 ${getSize(90)};
+`;
+
+const SAddress = styled.div`
+  ${flex("column", "", "")};
+  /* ${defaultTxtStyle("", "20", "")}; */
+  padding: ${getSize(40)} 0 ${getSize(40)};
+`;
+
+const SHallName = styled.p`
+  font-size: ${getVwSize(24)};
+
+  @media screen and (min-width: 640px) {
+    font-size: ${getSize(24)};
+  }
+`;
+
+const SHallAddress = styled.p`
+  font-size: ${getVwSize(20)};
+  margin: ${getSize(10)} 0;
+
+  @media screen and (min-width: 640px) {
+    font-size: ${getSize(20)};
+  }
+`;
+
+const SMap = styled.div`
+  ${boxSize("100vw", "35rem")};
+  background-color: black;
+  ${paddingIgnore};
+  margin-bottom: ${getVwSize(60)};
+  @media screen and (min-width: 640px) {
+    ${boxSize("64rem", "35rem")};
+    margin-bottom: ${getSize(60)};
+  }
 `;
